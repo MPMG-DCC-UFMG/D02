d09ce5daa2245a850a81afec6bffbcb747b3baf3
Gabriel Oliveira <gabrielpdoliveira@gmail.com>
2022-08-17 19:03:12 -0300

    updating expect_compound_columns_to_be_unique_custom_message

--- expectation/custom/expect_compound_columns_to_be_unique_custom_message.py
+++ expectation/custom/expect_compound_columns_to_be_unique_custom_message.py
@@ -10,6 +10,7 @@ from great_expectations.render.util import (
     parse_row_condition_string_pandas_engine,
     substitute_none_for_missing,
 )
+from great_expectations.expectations.core import ExpectCompoundColumnsToBeUnique
 
 
 class ExpectCompoundColumnsToBeUniqueCustomMessage(ExpectCompoundColumnsToBeUnique):
